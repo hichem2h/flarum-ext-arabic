e392e90fce80385266f734b47b5c097cb35d6711
Hichem2h <hichem2h@gmail.com>
2018-04-03 14:26:38 +0100

    remove cache when change language

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,7 @@
 
 use Flarum\Event\ConfigureLocales;
 use Flarum\Event\ConfigureClientView;
+use Flarum\Forum\WebApp;
 use Flarum\Locale\LocaleManager;
 use Illuminate\Contracts\Events\Dispatcher;
 
@@ -12,6 +13,9 @@ return function (Dispatcher $events) {
 
     //Add assets
     $events->listen(ConfigureClientView::class,  function(ConfigureClientView $event) {
+      $webapp = app(WebApp::class);
+	$assets = $webapp-> getAssets();
+	$assets->flushLocaleCss();
       $locales = app(LocaleManager::class);
 
       if ($event->isForum() && $locales->getLocale() == "ar") {
